1ed0f00162c5fd9697ab7113f3a555e1a030403d
AdamRse <bakaarion3@gmail.com>
2024-08-07 17:13:06 +0200

    ++

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,11 @@ use App\Http\Controllers\JourneyController;
 
 // Route temporaire pour tester sans token
 Route::get('/users/nt', [UserController::class, 'index']);
-
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/users', [UserController::class, 'create']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
-    Route::post('/users', [UserController::class, 'create']);
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
